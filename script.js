db660b12139939b69d3144e77f65f7d12b774c11
João Oliveira <jaoovit@gmail.com>
2023-11-03 11:33:37 +0000

    fix the initial slide.value textContent

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function slideControl() {
     let slide = document.querySelector('#slide')
     let show = document.querySelector('#show')
+    
 
-    show.textContent = slide.value
-
+    show.textContent = slide.value + " X " + slide.value
+    
     slide.addEventListener('input', (event) => {
         show.textContent = event.target.value + " X " + event.target.value
     })
+
 }
 
 slideControl()
+
+let squareToSquares = document.querySelector('#squaretosquares')
+let square = document.createElement('div')
+square.innerText = 'square' 
+
+document.squareToSquares.appendChield(square)
