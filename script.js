54f0231610bfaa08ea98c98926d995bbe9109de6
João Oliveira <jaoovit@gmail.com>
2023-11-07 12:57:07 +0000

    create a 16x16 grid

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-function slideControl() {
-    let slide = document.querySelector('#slide')
-    let show = document.querySelector('#show')
-    let squareToSquares = document.querySelector('#squaretosquares');
-    let square = document.createElement('div');
+const grid = document.querySelector('#grid');
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        grid.appendChild(cell);
+    }
+}
     
-    squareToSquares.appendChild(square);
+
+function slideControl() {
+    let slide = document.querySelector('#slide');
+    let show = document.querySelector('#show');
+    
     show.textContent = slide.value + " X " + slide.value
     
     slide.addEventListener('input', (event) => {
-        show.textContent = event.target.value + " X " + event.target.value
+        show.textContent = event.target.value + " X " + event.target.value        
     })
 
 }
 
 slideControl()
+
 
 
 
