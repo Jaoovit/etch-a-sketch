9f7c24997b63a19035f4e903245b5bec6ad0fe6f
João Oliveira <jaoovit@gmail.com>
2023-11-08 12:08:28 +0000

    create a system to pass the slide value to the grid

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-const grid = document.querySelector('#grid');
-
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        grid.appendChild(cell);
-    }
-}
-    
-
 function slideControl() {
     let slide = document.querySelector('#slide');
     let show = document.querySelector('#show');
     
-    show.textContent = slide.value + " X " + slide.value
+    
+    show.textContent = slide.value + " X " + slide.value 
     
     slide.addEventListener('input', (event) => {
-        show.textContent = event.target.value + " X " + event.target.value        
+        show.textContent = event.target.value + " X " + event.target.value
+        
+        incriseSquare(event.target.value)
     })
-
+    
 }
 
 slideControl()
 
+
+function incriseSquare(slideValue) {
+    console.log(slideValue)
+    let grid = document.querySelector('#grid');
+
+    for (let i = 0; i < slideValue; i++) {
+        for (let j = 0; j < slideValue; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            grid.appendChild(cell);
+        }
+    }
+}
 
 
 
