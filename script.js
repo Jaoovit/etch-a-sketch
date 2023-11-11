7cb90540f060d67198716c36edadbbbd7f5f3898
João Oliveira <jaoovit@gmail.com>
2023-11-11 13:15:21 +0000

    create a design to select and deselect the buttons

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ function incriseSquare(size) {
 
     for (let i = 0; i < size * size; i++) {
             let cell = document.createElement('div');
+            cell.classList.add('cell')
+            cell.setAttribute('id', 'cell')
             grid.appendChild(cell);
         }
     }
@@ -32,30 +34,50 @@ let rainbowBtn = document.querySelector('#rainbow')
 let eraseBtn = document.querySelector('#erase')
 let clear = document.querySelector('#clean')
 let colorCircle = document.querySelector('#colorcircle')
+let cell = document.querySelector('#cell')
 
 
 colorBtn.addEventListener('click', function onClick(event) {
      event.target.style.backgroundColor = '#313131';
      event.target.style.color = 'white';
      event.target.style.borderColor = 'white';
+     
+     rainbowBtn.style = ""
+     eraseBtn.style = ""
+     clear.style = ""
+     
+
+     cell.style.backgroundColor = '#313131';
 })
 
 rainbowBtn.addEventListener('click', function onClick(event) {
     event.target.style.backgroundColor = '#313131';
     event.target.style.color = 'white';
     event.target.style.borderColor = 'white';
+
+     colorBtn.style = ""
+     eraseBtn.style = ""
+     clear.style = ""
 })
 
 eraseBtn.addEventListener('click', function onClick(event) {
     event.target.style.backgroundColor = '#313131';
     event.target.style.color = 'white';
     event.target.style.borderColor = 'white';
+
+     rainbowBtn.style = ""
+     colorBtn.style = ""
+     clear.style = ""
 })
 
 clear.addEventListener('click', function onClick(event) {
     event.target.style.backgroundColor = '#313131';
     event.target.style.color = 'white';
     event.target.style.borderColor = 'white';
+
+     rainbowBtn.style = ""
+     eraseBtn.style = ""
+     colorBtn.style = ""
 })
 
 
